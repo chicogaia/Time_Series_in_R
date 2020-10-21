84826bd5fed396216394853694384bae02d502b6
chicogaia <francisco.r.gaia@gmail.com>
2020-10-21 15:52:56 -0300

    Right before the real-world dataset

--- Time_Series_in_R.R
+++ Time_Series_in_R.R
@@ -38,7 +38,7 @@ ggAcf(dat$X,type="partial") + ggtitle("Partial Autocorrelation PACF Plot")
 
 
 
-##CHECKING FOR STATIONARITY
+### CHECKING FOR STATIONARITY
 
 #create three time series for example
 df<-sim.stationary.example(n=1000)
@@ -78,6 +78,79 @@ adf.test(df$X1)
 #in fact, NonStationary
 adf.test(df$X3)
 #Output is low p-value: we have evidence that X3 is Stationary
+
+
+### TRANSFORMING FOR STATIONARITY
+
+#Tipically, if we have a NonStationary time-series, we try and transform the data to make it Stationary by 
+##Differencing
+##or Detrending
+
+#Option 1: DIFFERENCING
+#Differencing involves taking differences between successive time series values; given that a Non-Stationary time-series
+#presents with added normally distributed (random) errors (epsilons) across successive data-points, taking these errors constructs
+#a Stationary time-series
+#In other words, the time-series of a NonStationary time-series' epsilons is a Stationary time-series!
+
+#difference time series to make stationary: the code below will substract from each value of X1 its previous lagged 
+#of lag = 1 (lag function in the package "dplyr")
+diff <- df$X1 - lag(df$X1,1)
+
+#plot original and differenced time series in ACF plots
+g1 <- ggAcf(df$X1, type = "correlation") + ggtitle("ACF for Original time-series")
+g2 <- ggAcf(diff, type = "correlation") + ggtitle("ACF for differeced time-series")
+grid.arrange(g1,g2)
+
+#Option 2: DETRENDING
+#Detrending involves removing a deterministic relationship with time from the time-series.
+#So, the trended time-series is one that has a trend with time built into it - i.e. it can be described in a
+#linear model with independent variable t (time) and coefficient B
+#A trending time-series, therefore, can be described as Y(t) = B*t + epsilon
+#Thus, detrending this function means arriving at: Y_det(t) = Yt - B*t = epsilon
+#i.e., taking the residuals from that fitted linear model with time to be the new Stationary time-series.
+
+#detrend time series to make stationary
+detrended <- resid(lm(X2 ~ t, df))
+
+#plot original and differenced time series in ACF plots
+g1 <- ggAcf(df$X2, type = "correlation") + ggtitle("ACF for Original time-series")
+g2 <- ggAcf(detrended, type = "correlation") + ggtitle("ACF for differeced time-series")
+grid.arrange(g1,g2)
+
+### BASIC MODEL TYPES
+
+##Autoregressive Models AR(p)
+#(p = number of AR lags)
+#y(t) = Intercept + Coefficient1*y(t-1) + ... + Coefficientn*y(t-n) + epsilon
+
+##Moving Average Models MA(q)
+#(q = number of MA lags)
+
+##Autoregressive Moving Average Models ARMA(p,q)
+#Combination of AR and MA models
+
+##Autoregressive Integrated Moving Average Models ARIMA(p,d,q)
+#ARMA model with differencing 
+#(d = number of differences to use)
+
+##Decomposition Models
+#They specifiy Y(t) as a combination of three components:
+#a trend component (T)
+#a seasonal component (S)
+#an error component (E)
+#So Y(t) = f(T(t) , S(t) , E(t))
+#There are many different models to use when estimating these components. Here, we will see STL Model for its ease
+#of use and flexibility
+
+### FITTING MODELS WITH THE BOX JENKINS METHOD
+
+##Steps
+#Identify whether the time-series is Stationary
+#Identify p, d, and q of the time-series by
+        #Making the tim-series stationary through differencing/detrending to find d
+        #
+
+
 
 
 
